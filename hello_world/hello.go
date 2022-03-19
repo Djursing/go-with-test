6725345308b71514c343cb18ab646781ffac386d
Djursing <oliverdjursing@gmail.com>
2022-03-19 19:55:15 +0100

    finish hello world secion

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -1,17 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	return englishHelloPrefix + name
+	return greedingPrefix(language) + name
+}
+
+func greedingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+
+	case french:
+		prefix = frenchHelloPrefix
+
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("", ""))
 }
