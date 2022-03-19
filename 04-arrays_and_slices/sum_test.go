a3eacf75f18d8f145e96e9d41a13e065e4cb0761
Djursing <oliverdjursing@gmail.com>
2022-03-19 21:49:46 +0100

    remove redundant test

--- 04-arrays_and_slices/sum_test.go
+++ 04-arrays_and_slices/sum_test.go
@@ -3,12 +3,15 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	nums := [5]int{1, 2, 3, 4, 5}
 
-	got := Sum(nums)
-	exp := 15
+	t.Run("collection of any size", func(t *testing.T) {
+		nums := []int{1, 2, 3}
 
-	if got != exp {
-		t.Errorf("expected %d want %d given, %v", exp, got, nums)
-	}
+		got := Sum(nums)
+		exp := 6
+
+		if got != exp {
+			t.Errorf("expected %d, want %d, given %v", exp, got, nums)
+		}
+	})
 }
